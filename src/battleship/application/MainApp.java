af1c8cdc76852a0f09b7f5bf89df3359f4acdf06
wantczak <antek10001@gmail.com>
2016-04-24 19:52:09 +0200

    Commit - Network Changes, Board

--- src/battleship/application/MainApp.java
+++ src/battleship/application/MainApp.java
@@ -7,7 +7,11 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
+/**
+ * Glowna klasa tworzaca
+ * @author Wojciech Antczak
+ *
+ */
 public class MainApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
