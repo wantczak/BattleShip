a5e85fd3d770c415887b1a68f2de024a908751c5
wantczak <antek10001@gmail.com>
2016-05-08 19:51:17 +0200

    Commit

--- src/battleship/application/MainApp.java
+++ src/battleship/application/MainApp.java
@@ -3,6 +3,7 @@ package battleship.application;
 import battleship.gui.menu.MenuViewController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -18,6 +19,7 @@ public class MainApp extends Application {
 	        Scene scene = new Scene(menuViewController.getView(), 800, 600);
 	        scene.getStylesheets().add("/css/application.css");
 	        primaryStage.setScene(scene);
+	        primaryStage.initStyle(StageStyle.UNDECORATED);
 	        primaryStage.setMaximized(true);
 	        primaryStage.setTitle("Battle S.H.I.P.");
 	        primaryStage.show();
